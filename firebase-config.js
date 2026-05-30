93b3082297217b12a5578d3cf2311335792d65f3
yacinesibawais-droid <yacine.sibawais@gmail.com>
2026-05-30 23:48:58 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID"
+};
